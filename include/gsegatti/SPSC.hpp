d04a0ba1d5429734c02272a9cfbc6d1ca2966125
Gabriel Segatti <gabrielsegatti2gmail.com>
2024-07-15 18:34:02 -0300

    more progress

--- include/gsegatti/SPSC.hpp
+++ include/gsegatti/SPSC.hpp
@@ -1,5 +1,5 @@
-// #ifndef SPSC_HPP
-// #define SPSC_HPP
+#ifndef GSEGATTI_SPSC
+#define GSEGATTI_SPSC
 
 #include <type_traits>
 #include <concepts>
@@ -18,26 +18,14 @@ namespace gsegatti
   public:
     void push(const T &t) noexcept
     {
-      size_t writeIdx = writeIdx_.load(std::memory_order_relaxed);
-      size_t nextWriteIdx = (writeIdx + 1) % actualSize;
-      while (nextWriteIdx == readIdxCached_)
-      {
-        readIdxCached_ = readIdx_.load(std::memory_order_relaxed);
-      }
-      block[writeIdx] = t;
-      writeIdx_.store(nextWriteIdx, std::memory_order_release);
+      insert(t);
     }
 
-    void push(const T &&t) noexcept
+    template<typename TT>
+    requires std::is_nothrow_move_assignable_v<TT>
+    void push(TT &&t) noexcept
     {
-      size_t writeIdx = writeIdx_.load(std::memory_order_relaxed);
-      size_t nextWriteIdx = (writeIdx + 1) % actualSize;
-      while (nextWriteIdx == readIdxCached_)
-      {
-        readIdxCached_ = readIdx_.load(std::memory_order_relaxed);
-      }
-      block[writeIdx] = t;
-      writeIdx_.store(nextWriteIdx, std::memory_order_release);
+      insert(std::forward<T>(t));
     }
 
     [[nodiscard]] T *front() noexcept
@@ -74,12 +62,30 @@ namespace gsegatti
     [[nodiscard]] constexpr size_t capacity() const noexcept { return queueSize; }
 
   private:
+    template <typename U>
+    void insert(U &&t)
+    {
+      size_t writeIdx = writeIdx_.load(std::memory_order_relaxed);
+      size_t nextWriteIdx = (writeIdx + 1) % actualSize;
+      while (nextWriteIdx == readIdxCached_)
+      {
+        readIdxCached_ = readIdx_.load(std::memory_order_relaxed);
+      }
+      block[writeIdx] = std::forward<U>(t);
+      writeIdx_.store(nextWriteIdx, std::memory_order_release);
+    }
+
+    // Reduce False Sharing via alignas.
+    // Need an extra space, otherwise `push()` will get stuck.
     alignas(cacheLineAlignment) static constexpr size_t actualSize = queueSize + 1;
     alignas(cacheLineAlignment) T block[actualSize];
-    // Reduce False Sharing.
     alignas(cacheLineAlignment) std::atomic<size_t> writeIdx_ = {0};
     alignas(cacheLineAlignment) std::atomic<size_t> readIdx_ = {0};
+    // Reduce cache coherency traffic if there's no need to fetch the current values of writeIdx_ and readIdx_.
+    // As seen on https://rigtorp.se/ringbuffer/.
     alignas(cacheLineAlignment) size_t readIdxCached_ = 0;
     alignas(cacheLineAlignment) size_t writeIdxCached_ = 0;
   };
 }
+
+#endif
--- src/Example.cpp
+++ src/Example.cpp
@@ -2,23 +2,34 @@
 #include <iostream>
 #include <thread>
 
-int main(int argc, char *argv[])
+int main()
 {
-  (void)argc, (void)argv;
 
   using namespace gsegatti;
 
+  // Create a queue of size 1 and insert elements over and over again.
   SPSC<int, 1> q;
-  q.push(1);
+  size_t iterations = 1000000;
+
   auto t = std::thread([&]
-    {
-      while (!q.front())
-        ;
-      std::cout << *q.front() << std::endl;
-      q.pop();
-    });
+                       {
+      for(int i = 0; i < iterations; ++i){
+        // Wait while write thread has yet to begin writing.
+        while (!q.front())
+          ;
+        auto frontELement = q.front();
+        // Ensure our structure is working.
+        if(!frontELement || *frontELement != i){
+          throw std::runtime_error("Wrong ordering of elements being popped!");
+        }
+        q.pop();
+      } });
+
+  for (size_t i = 0; i < iterations; ++i)
+  {
+    q.push(i);
+  }
 
   t.join();
-
   return 0;
 }
